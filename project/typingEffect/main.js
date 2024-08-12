96b1cf853cb1d1db3a9da7f15d14eb3e91e00cd1
natista99 <natista99@gmail.com>
2024-08-13 00:22:33 +0900

    event

--- project/typingEffect/main.js
+++ project/typingEffect/main.js
@@ -1,10 +1,10 @@
 
 let target = document.querySelector("#dynamic");
 
+console.log("target:"+target);
 function blink(){
-   
    target.classList.toggle("active");
 }
 
-setInterval(blink, 1000);
+setInterval(blink, 500);
 //setInterval 무한 반복, setTimeout 한번 실행
