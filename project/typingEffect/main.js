da2392570da1fca9ed6757df54497d2b0ba884bb
kimhj <natista99@gmail.com>
2024-08-12 20:32:09 +0900

    toggle

--- project/typingEffect/main.js
+++ project/typingEffect/main.js
@@ -1,3 +1,10 @@
 
 let target = document.querySelector("#dynamic");
 
+function blink(){
+   
+   target.classList.toggle("active");
+}
+
+setInterval(blink, 1000);
+//setInterval 무한 반복, setTimeout 한번 실행
